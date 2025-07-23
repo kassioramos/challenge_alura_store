59f64ac190831de70cd82ee54ab5864ca10b3357
kassioramos <kassio.ramos03@gmail.com>
2025-07-22 22:04:17 -0300

    Concatenar os dados das lojas

--- analisar_dados.py
+++ analisar_dados.py
@@ -5,18 +5,17 @@ url2 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-scienc
 url3 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science/refs/heads/main/base-de-dados-challenge-1/loja_3.csv"
 url4 = "https://raw.githubusercontent.com/alura-es-cursos/challenge1-data-science/refs/heads/main/base-de-dados-challenge-1/loja_4.csv"
 
-loja = pd.read_csv(url)
+#Lendo dados
+loja1 = pd.read_csv(url)
 loja2 = pd.read_csv(url2)
 loja3 = pd.read_csv(url3)
 loja4 = pd.read_csv(url4)
 
 
-#saida
-print("loja 1:")
-print(loja.head())
-print("\nloja 2:")
-print(loja2.head())
-print("\nloja 3:")
-print(loja3.head())
-print("\nloja 4:")
-print(loja4.head())
+#Conectar Dataframes
+todas_lojas = pd.concat([loja1, loja2, loja3, loja4], ignore_index=True)
+
+#exibir registros do Dataframe
+print("Dados de todas a lojas:")
+print(todas_lojas.head())
+
